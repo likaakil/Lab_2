03498969387ad2c97671fafa9a5d98bb534993c3
likaakil <48705121+likaakil@users.noreply.github.com>
2019-06-11 01:47:35 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,76 @@
+/*******************************************************************************
+*                   : TUSUR
+* File              : main.c
+* Compiler          : IAR for ARM 
+* Version           : 8.32
+* Last modified     : 04.06.2019
+* 
+* Author            : Goncharova Anzhelika
+* Support mail      : lika_tsk@mai.ru
+* 
+* Description       : Work with stack
+*
+********************************************************************************/
+// For run to needed code, you must uncomment needed #define
+//#define RECURSIVEFUNC
+//#define CHANGESP
+//#define ADDSP
+//#define ARRAYTOFUNC
+//#define PARAMSTOFUNC
+#define TURNSP
+  
+int func(void)
+{
+  return func();
+}
+
+int func2(int a[5])
+{
+  return (a[0]+a[1]+a[2]+a[3]+a[4]);
+}
+
+int func3(int a, int b, int c, int d, int e, int f)
+{
+  return (a+b+c+d+e+f);    
+}
+
+void main(void)
+{
+  #ifdef RECURSIVEFUNC
+  {
+    func(); // Recursive function (WARNING)
+  }
+  #endif
+  
+  #ifdef CHANGESP
+  {
+    __asm( "MOV R13, #10"); // R13 = 10 (ERROR)
+  }
+  #endif
+  
+  #ifdef ADDSP
+  {
+    __asm( "MOV R1, #9999\n" "ADD R13, R1"); // R1 = 9999, R13=(R1+R13) (WARNING)
+  }
+  #endif
+  
+  #ifdef ARRAYTOFUNC
+  {
+    int array[5] = {1,2,3,4,5}; // Stack params
+    func2(array);
+  }
+  #endif
+  
+  #ifdef PARAMSTOFUNC
+  {
+    int sum = func3(16,32,48,64,80,96); // Stack params
+  }
+  #endif
+  
+  #ifdef TURNSP
+  {
+    __asm( "MOV R0, #0x2\n" "MSR CONTROL, R0"); // Two stack pointer
+  }
+  #endif
+}
+
